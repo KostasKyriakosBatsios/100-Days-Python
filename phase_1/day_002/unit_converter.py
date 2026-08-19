9f624af52893e2e87a1f4a34d9edcb37089b0bbc
KostasKyriakosBtaios <konstantinos.mpatsios@gmail.com>
2026-08-19 21:44:54 +0300

    Day 002 - Unit Converter: The user is being asked to choose a category between Length, Temperature and Weight, and then for each category the user has to choose an available conversion. Afterwards, the user has to enter a value, the conversion is done, and it continues until the user doesn't want to (answers n in the question about making another conversion)

---
+++ phase_1/day_002/unit_converter.py
@@ -0,0 +1,117 @@
+# Day 2 19/8/26!
+
+def main():
+    while True:
+        print("=== Unit Converter ===\n")
+
+        # Ask the user which category wants to choose
+        category = get_answer("1. Length\n2. Temperature\n3. Weight\n\nChoose category: ")
+
+        # Check based on the category, in order to make the proper conversions
+        if category == 1:
+            convert_length("\nChoose conversion: \n1. Kilometers → Miles\n2. Miles → Kilometers\n" \
+            "3. Meters → Feet\n4. Feet → Meters\n\nChoose: ")
+        elif category == 2:
+            convert_temperature("Choose conversion: \n1. Celsius → Fahrenheit\n2. Fahrenheit → Celsius\n\nChoose: ")
+        else:
+            convert_weight("Choose conversion: \n1. kg → pounds\n2. pounds → kg\n\nChoose: ")
+
+        # Ask if the user wants to continue
+        continuing = ""
+        while continuing not in ["y","n"]:
+            continuing = input("Do you want to make another conversion? (y/n): ").lower()
+
+        if continuing == "n":
+            print("\nGoodbye!")
+            break
+
+# Function that gets the answer of which category the user wants to use
+def get_answer(prompt):
+    while True:
+        try:
+            a = int(input(prompt))
+            if a not in [1,2,3]:
+                print("\nInvalid option\n")
+                continue
+        except ValueError:
+            print()
+            continue
+        break
+
+    return a
+
+# Function that converts category length
+def convert_length(prompt):
+    while True:
+        try:
+            a = int(input(prompt))
+            if a not in [1,2,3,4]:
+                print("\nInvalid option\n")
+                continue
+        except ValueError:
+            print()
+            continue
+        break
+
+    while True:
+        try:
+            val = float(input("\nEnter value: "))
+        except ValueError:
+            print()
+            continue
+        break
+
+    if a == 1: print(f"\nResult: {(val*0.621371):.1f} miles\n")
+    elif a == 2: print(f"\nResult: {(val/0.621371):.1f} kilometers\n")
+    elif a == 3: print(f"\nResult: {(val*3.28084):.1f} feet\n")
+    else: print(f"\nResult: {(val/3.28084):.1f} meters\n")
+
+# Function that converts category temperature
+def convert_temperature(prompt):
+    while True:
+            try:
+                a = int(input(prompt))
+                if a not in [1,2]:
+                    print("\nInvalid option\n")
+                    continue
+            except ValueError:
+                print()
+                continue
+            break
+    
+    while True:
+        try:
+            val = float(input("\nEnter value: "))
+        except ValueError:
+            print()
+            continue
+        break
+
+    if a == 1: print(f"\nResult: {((val*1.8)+32):.1f} F°\n")
+    else: print(f"\nResult: {((val-32)/1.8):.1f} C°\n")
+
+# Function that converts category weight
+def convert_weight(prompt):
+    while True:
+            try:
+                a = int(input(prompt))
+                if a not in [1,2]:
+                    print("\nInvalid option\n")
+                    continue
+            except ValueError:
+                print()
+                continue
+            break
+    
+    while True:
+        try:
+            val = float(input("\nEnter value: "))
+        except ValueError:
+            print()
+            continue
+        break
+
+    if a == 1: print(f"\nResult: {(val*2.20462):.1f} pounds\n")
+    else: print(f"\nResult: {(val/2.20462):.1f} kg\n")
+
+main()
